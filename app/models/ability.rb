aa22ea5e21ccf6005bb75df2c1b976fb968f6bdd
Peter Hankiewicz <peter.hankiewicz@gmail.com>
2020-04-23 13:25:36 +0200

    Only admins should be able to create instances

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -18,14 +18,8 @@ class Ability
       can :create_room_in, Instance do |instance|
         ((instance.moderators.include?(user) || user == instance.owner || !instance.closed)) && (instance.private == false || instance.access_token == params[:access_token])
       end
-      can :update, Instance do |instance|
-        user == instance.owner
-      end
       can :toggle_open_topic, Instance do |instance|
         user == instance.owner || instance.moderators.include?(user)
-      end
-      can :set_moderators, Instance do |instance|
-        user == instance.owner
       end
       can :read, Room do |room|
         room.instance.private == false || room.instance.access_token == params[:access_token] || [room.instance.owner, room.owner].include?(user)
@@ -35,13 +29,6 @@ class Ability
       end
       can :destroy, Room do |room|
         [room.instance.owner, room.owner].include?(user)
-      end
-    end
-
-    if user.role?(Role.owner) || user.role?(Role.registered)
-      can :create, Instance
-      can :destroy, Instance do |instance|
-        user == instance.owner
       end
     end
   end
